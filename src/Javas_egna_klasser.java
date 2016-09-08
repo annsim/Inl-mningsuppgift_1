18881a1eb7e69d7534083528e77fb6c7098a45f6
annsim <anna.simonsson@comhem.com>
2016-09-08 16:25:42 +0200

    Klart

--- src/Javas_egna_klasser.java
+++ src/Javas_egna_klasser.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
 
 public class Javas_egna_klasser {
+	
+	public static void main(String[] args){
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Ange triangelns höjd: ");
+		String heigth = scan.nextLine();
+		System.out.print("Ange basen på triangeln: ");
+		String base = scan.nextLine();
+		
+		scan.close();
+		
+		double doubleHeigth = Double.parseDouble(heigth);
+		double doubleBase = Double.parseDouble(base);
+			
+		double hypotenuse = Math.hypot(doubleHeigth, doubleBase);
+		System.out.print("Hypotenusan av triangeln: "+hypotenuse);
+		
+		
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+			
 	}
 
 }
